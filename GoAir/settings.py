d8d0894be86013bc52b407db239734ae500a9e3d
Junlu Huang <2994936h@student.gla.ac.uk>
2025-03-19 23:01:41 +0000

    2301  fix

--- GoAir/settings.py
+++ GoAir/settings.py
@@ -20,7 +20,7 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(file).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
